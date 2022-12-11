269ac0a164c7e4050bc71b605e1c7d656efde992
unknown <odinzero@outlook.com>
2022-12-11 22:27:32 +0800

    chore: 使用不定冠词修饰参数

--- src/chapter01/statement.03.ts
+++ src/chapter01/statement.03.ts
@@ -48,22 +48,22 @@ function statement(invoice: Invoice, plays: Plays) {
 	return result;
 }
 
-function amountFor(aPerformance: PlayPerformance, play: PlayValue) {
+function amountFor(perf: PlayPerformance, play: PlayValue) {
 	let result = 0;
 
 	switch (play.type) {
 		case 'tragedy':
 			result = 40_000;
-			if (aPerformance.audience > 30) {
-				result += 1_000 * (aPerformance.audience - 30);
+			if (perf.audience > 30) {
+				result += 1_000 * (perf.audience - 30);
 			}
 			break;
 		case 'comedy':
 			result = 30_000;
-			if (aPerformance.audience > 20) {
-				result += 10_000 + 500 * (aPerformance.audience - 20);
+			if (perf.audience > 20) {
+				result += 10_000 + 500 * (perf.audience - 20);
 			}
-			result += 300 * aPerformance.audience;
+			result += 300 * perf.audience;
 			break;
 		default:
 			throw new Error(`unknown type: ${play.type}`);
