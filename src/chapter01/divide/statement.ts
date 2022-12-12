2c3445e0220296034a9d4db1a448ac75f9678802
unknown <odinzero@outlook.com>
2022-12-12 23:08:44 +0800

    feat: 添加htmlstatement

--- src/chapter01/divide/statement.ts
+++ src/chapter01/divide/statement.ts
@@ -27,18 +27,15 @@ export type Plays = {
 };
 
 function statement(invoice: Invoice, plays: Plays) {
-	return renderPlainText(createStatementData(invoice, plays), plays);
+	return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderPlainText(
-	data: {
-		customer: string;
-		performances: RichPlayPerformance[];
-		totalVolumeCredit: number;
-		totalAmount: number;
-	},
-	plays: Plays
-) {
+function renderPlainText(data: {
+	customer: string;
+	performances: RichPlayPerformance[];
+	totalVolumeCredit: number;
+	totalAmount: number;
+}) {
 	let result = `Statement for ${data.customer}\n`;
 	for (const perf of data.performances) {
 		// print line for this order
@@ -49,14 +46,37 @@ function renderPlainText(
 	result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
 	result += `You earned ${data.totalVolumeCredit} credits\n`;
 	return result;
+}
 
-	// function format(aNumber: number) {
-	function usd(aNumber: number) {
-		return new Intl.NumberFormat('en-US', {
-			style: 'currency',
-			currency: 'USD',
-			minimumFractionDigits: 2
-		}).format(aNumber);
+function htmlStatement(invoice: Invoice, plays: Plays) {
+	return renderHTML(createStatementData(invoice, plays));
+}
+
+function renderHTML(data: {
+	customer: string;
+	performances: RichPlayPerformance[];
+	totalVolumeCredit: number;
+	totalAmount: number;
+}) {
+	let result = `<h1>Statement for ${data.customer}</h1>\n`;
+	result += '<table>\n';
+	for (const perf of data.performances) {
+		// print line for this order
+		result += `<tr><td>${perf.play.name} </td><td> ${perf.audience} seats</td>`;
+		result += `<td>${usd(perf.amount)}</td></tr>\n`;
 	}
+	result += '</table>\n';
+	result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+	result += `<p>You earned <em>${data.totalVolumeCredit}</em> credits</p>\n`;
+	return result;
+}
+
+// function format(aNumber: number) {
+function usd(aNumber: number) {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2
+	}).format(aNumber / 100);
 }
 export default statement;
